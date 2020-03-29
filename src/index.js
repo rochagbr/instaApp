ad1a903a985541ee05ad0d692d28ff59d47eaffb
Gabriel <bieloky@gmail.com>
2020-03-29 12:40:45 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+export default function Main() {
+  return (
+    <SafeAreaView>
+      <Text>Helloo</Text>
+    </SafeAreaView>
+  );
+}
