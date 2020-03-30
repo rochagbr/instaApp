90640fe6823753ffceccc7a5444c78a48f0b2569
Gabriel <bieloky@gmail.com>
2020-03-30 01:19:39 -0300

    Add Logo

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,19 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
+import { Image } from 'react-native';
 import Feed from './pages/Feed';
+import logo from './assets/instagram.png';
+
 const { Navigator, Screen } = createStackNavigator();
 
 const Routes = () => (
   <NavigationContainer>
-    <Navigator>
+    <Navigator
+      screenOptions={{
+        headerTitle: <Image source={logo} />,
+        headerTitleAlign: 'center',
+      }}>
       <Screen name="Feed" component={Feed} />
     </Navigator>
   </NavigationContainer>
